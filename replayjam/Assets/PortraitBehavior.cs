c067fbe648ee50db9099361e3450b35d976ac67a
Sean Smith <seansmith@fedex.com>
2017-06-25 11:41:45 -0400

    Working on portraits

---
+++ replayjam/Assets/PortraitBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PortraitBehavior : MonoBehaviour {
+    
+    public List<Image> portraits;
+    public List<Image> glassStages;
+
+    private RectTransform rt;
+
+    // Slide Variables
+    private float startTime;
+    public Vector2 startPos;
+    public Vector2 endPos;
+    public float slideTime;
+    public AnimationCurve slideCurve;
+
+    // Use this for initialization
+    void Start () {
+        startTime = Time.time;
+        rt = GetComponent<RectTransform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float percent = slideCurve.Evaluate((Time.time - startTime) / slideTime);
+        Debug.Log("Percent: " + percent);
+        rt.position = Vector2.Lerp(startPos, endPos, percent);
+	}
+}
